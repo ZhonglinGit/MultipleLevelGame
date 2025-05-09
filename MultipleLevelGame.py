8295abd4c3cd4f84ce880f44f4c75be5a1fe6103
zhonglin.wang <150401182+ZhonglinGit@users.noreply.github.com>
2025-05-09 13:19:29 -0400

    Create MultipleLevelGame.py
    forget to put it in

---
+++ MultipleLevelGame.py
@@ -0,0 +1,222 @@
+#NAME: Mr. B
+#DATE: May 8, 2025
+#This is a multi-room maze game that shows how we can form new levels
+
+import pygame
+import random
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+blue = (0, 0, 255)
+green = (0, 255, 0)
+red = (255, 0, 0)
+
+#same wall class as before, these are our boundaries
+class Wall(pygame.sprite.Sprite):
+
+  def __init__(self, x, y, width, height, colour):
+    pygame.sprite.Sprite.__init__(self)
+    self.image = pygame.Surface([width, height])
+    self.image.fill(colour)
+    self.rect = self.image.get_rect()
+    self.rect.x = x
+    self.rect.y = y
+
+#Same player that we've been using throughout
+class Player(pygame.sprite.Sprite):
+
+  def __init__(self, x, y):
+    pygame.sprite.Sprite.__init__(self)
+    self.image = pygame.Surface([20, 20])
+    self.image.fill(green)
+    self.rect = self.image.get_rect()
+    self.rect.x = x
+    self.rect.y = y
+    self.xvel = 0
+    self.yvel = 0
+
+  def changespeed(self, x, y):
+    self.xvel += x
+    self.yvel += y
+
+#This detects when we hit the walls
+  def update(self, walls):
+    self.rect.x += self.xvel
+    wall_hit_list = pygame.sprite.spritecollide(self, walls, False)
+    for block in wall_hit_list:
+      if self.xvel > 0:
+        self.rect.right = block.rect.left
+      else:
+        self.rect.left = block.rect.right
+
+    self.rect.y += self.yvel
+    wall_hit_list = pygame.sprite.spritecollide(self, walls, False)
+    for block in wall_hit_list:
+      if self.yvel > 0:
+        self.rect.bottom = block.rect.top
+      else:
+        self.rect.top = block.rect.bottom
+
+#basic room class, with list of walls present
+class Room():
+  wall_list = []
+
+  def __init__(self):
+    self.wall_list = pygame.sprite.Group()
+
+#we make a specific class for each room
+class Room1(Room):
+  def __init__(self):
+    Room.__init__(self)
+
+    walls = [ [0, 0, 20, 170, black],
+              [0, 210, 20, 190, black],
+              [0, 0, 600, 20, black],
+              [0, 380, 600, 20, black],
+              [580, 0, 20, 170, black],
+              [580, 210, 20, 190, black],
+              [250, 200, 20, 110, black]
+            ]
+
+    for item in walls:
+    #item[0] = x, item[1] = y, item[2] = width, item[3] = ht, item[4] = colour
+      wall = Wall(item[0], item[1], item[2], item[3], item[4])
+      self.wall_list.add(wall)
+    #quickest way to create all the walls 
+
+class Room2(Room):
+  def __init__(self):
+    Room.__init__(self)
+
+    walls = [ [0, 0, 20, 170, blue],
+              [0, 210, 20, 190, blue],
+              [0, 0, 600, 20, blue],
+              [0, 380, 600, 20, blue],
+              [580, 0, 20, 170, blue],
+              [580, 210, 20, 190, blue],
+              [80, 50, 20, 310, blue],
+              [500, 50, 20, 310, blue]
+            ]
+
+    for item in walls:
+      wall = Wall(item[0], item[1], item[2], item[3], item[4])
+      self.wall_list.add(wall)
+
+class Room3(Room):
+  def __init__(self):
+    Room.__init__(self)
+
+    walls = [ [0, 0, 20, 170, red],
+              [0, 210, 20, 190, red],
+              [0, 0, 600, 20, red],
+              [0, 380, 600, 20, red],
+              [580, 0, 20, 170, red],
+              [580, 210, 20, 190, red],
+              [250, 200, 20, 110, red],
+              [350, 90, 20, 110, red]
+            ]
+
+    for item in walls:
+      wall = Wall(item[0], item[1], item[2], item[3], item[4])
+      self.wall_list.add(wall)
+
+pygame.init()
+screen = pygame.display.set_mode([600, 400])
+pygame.display.set_caption("Multiple Maze Madness")
+
+#starting location of player, right in middle of opening
+player = Player(10, 180)
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
+clock = pygame.time.Clock()
+
+done = False
+
+#create a list of rooms so we can access each 
+rooms = []
+
+#rooms[0]
+room = Room1()
+rooms.append(room)
+
+#rooms[1]
+room = Room2()
+rooms.append(room)
+
+#rooms[2]
+room = Room3()
+rooms.append(room)
+
+roomNum = 0     #starting room
+currentRoom = rooms[roomNum]
+
+while not done:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      done = True
+    elif event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_LEFT:
+        player.changespeed(-5, 0)
+
+      elif event.key == pygame.K_RIGHT:
+        player.changespeed(5, 0)
+
+      elif event.key == pygame.K_UP:
+        player.changespeed(0, -5)
+
+      elif event.key == pygame.K_DOWN:
+        player.changespeed(0, 5)
+
+    elif event.type == pygame.KEYUP:
+      if event.key == pygame.K_LEFT:
+        player.changespeed(5, 0)
+      elif event.key == pygame.K_RIGHT:
+        player.changespeed(-5, 0)
+      elif event.key == pygame.K_UP:
+        player.changespeed(0, 5)
+      elif event.key == pygame.K_DOWN:
+        player.changespeed(0, -5)
+
+  #updates the player with the walls of the current room to detect collision
+  player.update(currentRoom.wall_list)
+
+  #this handles when to swithf to the next room based on location of player
+  #first if controls when the player exits screen left
+  if player.rect.x < -10:
+    if roomNum == 0:
+      roomNum = 2
+      currentRoom = rooms[roomNum]
+      player.rect.x = 590
+    elif roomNum == 2:
+      roomNum = 1
+      currentRoom = rooms[roomNum]
+      player.rect.x = 590
+    elif roomNum == 1:
+      roomNum = 0
+      currentRoom = rooms[roomNum]
+      player.rect.x = 590
+  #first if controls when the player exits screen right
+  if player.rect.x > 590:
+    if roomNum == 0:
+      roomNum = 1
+      currentRoom = rooms[roomNum]
+      player.rect.x = -10
+    elif roomNum == 1:
+      roomNum = 2
+      currentRoom = rooms[roomNum]
+      player.rect.x = -10
+    elif roomNum == 2:
+      roomNum = 0
+      currentRoom = rooms[roomNum]
+      player.rect.x = -10
+
+  screen.fill(white)
+  all_sprites.draw(screen)
+  currentRoom.wall_list.draw(screen)
+
+  pygame.display.flip()
+  clock.tick(30)
+
+pygame.quit()
